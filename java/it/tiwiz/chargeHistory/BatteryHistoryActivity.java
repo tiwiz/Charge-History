8d37a8fed09a385e310ff3ad240c5c71aa14b536
tiwiz <roberto.orgiu@gmail.com>
2013-12-21 18:34:55 +0100

    Begins implementing more ActionBar buttons

--- java/it/tiwiz/chargeHistory/BatteryHistoryActivity.java
+++ java/it/tiwiz/chargeHistory/BatteryHistoryActivity.java
@@ -52,8 +52,11 @@ public class BatteryHistoryActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        switch(id){
+            case R.id.action_settings:
+                break;
+            case R.id.action_refresh:
+                break;
         }
         return super.onOptionsItemSelected(item);
     }
